1a7388867ff272d12ac4940836c325a7f5a4f09d
Hemantkumar Panchal <panchalhemant2001@yahoo.com>
2018-12-25 18:03:51 -0500

    php folder created for all php files

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8">
-<title>Welcome to Indus Florid</title>
-</head>
-
-<body>
-<h1>Welcome to Indus Florid</h1>
-<h2>This Site is under Construction</h2>
-</body>
-</html>
+<?php
+	header("Location: http://indusflorid.com:8082/php/home.php");
+?>
